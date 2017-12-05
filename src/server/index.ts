92f1591f9ccca8e12d26746c074789fa81e4344d
jonathanmourtada <jonathan.mourtada@divid.se>
2017-12-05 22:37:02 +0100

    add koa

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,20 @@
 import { LeaderBoardResponse } from "../shared/api-types";
 import { getLeaderBoard } from "./data";
+import * as Koa from "koa";
+import * as KoaStatic from "koa-static";
+import * as path from "path";
 
-(async () => {
-  const leaderboard = await getLeaderBoard();
+const app = new Koa();
 
-  Object.keys(leaderboard.members).forEach(index => {
-    const member = leaderboard.members[index];
-    console.log(`${member.name}: ${member.id}`);
-    console.log(`Stars: ${member.stars}`);
-    console.log(`Points: ${member.local_score}`);
-    console.log();
-  });
-})();
+const clientRoot = path.resolve("../client");
+app.use(KoaStatic(clientRoot));
+
+app.use(async (ctx, next) => {
+  if ((ctx.request.path = "/api/leaderboard")) {
+    ctx.body = await getLeaderBoard();
+  } else {
+    await next();
+  }
+});
+
+app.listen(3000);
